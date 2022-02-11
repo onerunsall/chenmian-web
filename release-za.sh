62f83d445ec656f1450ceef5cf94be333e4c25a8
renx <413038044@qq.com>
2022-02-11 10:00:18 +0800

    pack 更新

---
+++ release-za.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+echo "admin : asdfj12#!@jweoiqr$@1482DF3ewrjOY"
+echo
+
+domain=chenmian
+project=mumu
+domainPath=/root/data/domains/za
+
+versionNote=$1
+if [ -z "$versionNote" ]; then
+ versionNote='pack 更新'
+fi
+
+
+echo "-assign version"
+version=`date +%y%m%d%H`
+echo version: $version
+echo
+
+echo "-package"
+commitId=`git rev-parse --short HEAD`
+packageName="$domain-$project-web-$version-$commitId.zip"
+echo $packageName
+cd ./src
+../zip -q -r ../packages/$packageName ./
+cd ..
+echo
+
+echo "-git add"
+git add .
+echo
+
+echo "-git commit"
+git commit -am "$versionNote"
+echo
+
+echo "-git push"
+git push
+echo
+exit 0
+echo "-remote push"
+scp -P 22 ./dist/$packageName admin@47.110.157.60:$domainPath
+echo
+
+echo "-remote deploy"
+ssh -p 22 -t admin@47.110.157.60 "unzip -q -o $domainPath/$packageName -d $domainPath/webroot/$project"
+echo
+
+start https://njshangka.com/home
+
+echo success
